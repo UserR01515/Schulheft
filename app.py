e6391f551be148fb2c5590d32e04b9c71eb2bdba
UserR01515 <roncedricfrank@icloud.com>
2026-01-19 10:11:11 +0100

    Update app.py

--- app.py
+++ app.py
@@ -31,34 +31,34 @@ if psw == "192837465":
         st.sidebar.page_link("https://de.serlo.org/", label="SERLO Naturwissenschaften", icon="🔭")
 
     st.sidebar.feedback("stars")
-    ideen = st.sidebar.button("Ideen")
-    con = sqlite3.connect("Ideen")
-    cur = con.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS Ideen(Idee)")
-    if ideen is True: 
-        eingabe = st.sidebar.pills("Wähle aus:", ["Eintrag hinzufügen", "Ideen anzeigen", "Idee löschen"])
-        if eingabe == "Eintrag hinzufügen":
+    #ideen = st.sidebar.button("Ideen")
+    #con = sqlite3.connect("Ideen")
+    #cur = con.cursor()
+    #cur.execute("CREATE TABLE IF NOT EXISTS Ideen(Idee)")
+    #if ideen is True: 
+     #   eingabe = st.sidebar.pills("Wähle aus:", ["Eintrag hinzufügen", "Ideen anzeigen", "Idee löschen"])
+      #  if eingabe == "Eintrag hinzufügen":
             # dta = ["Vorlage", "Vorlage"]
             # cur.execute("INSERT INTO Hefteintrag_mathe VALUES(?, ?)", dta)
             # con.commit()
-            eintrag = st.text_input("Gebe hier zuerst einen Titel und dann deine Idee ein:")
+       #     eintrag = st.text_input("Gebe hier zuerst einen Titel und dann deine Idee ein:")
             #Eintrag bestätigen
-            if st.button("Eintrag bestätigen!"):
-                cur.execute(f"INSERT INTO Ideen VALUES('{eintrag}')")
-                data = cur.execute("SELECT * FROM Ideen")
-                con.commit()
-                st.table(data)
-        elif eingabe == "Ideen anzeigen":
-                data = cur.execute("SELECT * FROM Ideen")
-                con.commit()
-                st.table(data)
-        elif eingabe == "Idee löschen":
-            idee_löschen = cur.execute("SELECT datum FROM Hefteintrag_mathe")
-            auswahl_idee_löschen = st.selectbox("Wähle aus welchen Eintrag du löschen möchtest:", idee_löschen)
-            if st.button("Auswahl löschen"):
-                cur.execute(f"DELETE FROM Ideen WHERE datum = ('{auswahl_idee_löschen}')")
-                con.commit()
-                st.success("Idee wurde gelöscht")
+        #    if st.button("Eintrag bestätigen!"):
+         #       cur.execute(f"INSERT INTO Ideen VALUES('{eintrag}')")
+          #      data = cur.execute("SELECT * FROM Ideen")
+           #     con.commit()
+            #    st.table(data)
+        #elif eingabe == "Ideen anzeigen":
+         #       data = cur.execute("SELECT * FROM Ideen")
+          #      con.commit()
+           #     st.table(data)
+        #elif eingabe == "Idee löschen":
+         #   idee_löschen = cur.execute("SELECT datum FROM Hefteintrag_mathe")
+          #  auswahl_idee_löschen = st.selectbox("Wähle aus welchen Eintrag du löschen möchtest:", idee_löschen)
+           # if st.button("Auswahl löschen"):
+            #    cur.execute(f"DELETE FROM Ideen WHERE datum = ('{auswahl_idee_löschen}')")
+             #   con.commit()
+              #  st.success("Idee wurde gelöscht")
             
 #Mathe
     if auswahl == "Mathe":
